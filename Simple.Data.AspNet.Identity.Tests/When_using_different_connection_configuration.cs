759a004b572883eb7663e7c436e5a7a6ed6dfc50
NathanGloyn <nathans.dropbox@gmail.com>
2015-10-04 18:01:51 +0100

    Implement a regex to attempt idenitfy when the string provided is a connection string

--- Simple.Data.AspNet.Identity.Tests/When_using_different_connection_configuration.cs
+++ Simple.Data.AspNet.Identity.Tests/When_using_different_connection_configuration.cs
@@ -38,6 +38,18 @@ namespace Simple.Data.AspNet.Identity.Tests
         }
 
         [Test]
+        public void Should_create_UserStore_with_connecton_string()
+        {
+            var target = new UserStore<IdentityUser>(@"Server=(localdb)\v11.0;Database=IdentityTest;Integrated Security=true;Trusted_Connection=True;");
+            var task = target.FindByIdAsync(TestData.John_UserId);
+            task.Wait();
+
+            var user = task.Result;
+            Assert.That(user, Is.Not.Null);
+            Assert.That(user.Id, Is.EqualTo(TestData.John_UserId));
+        }
+
+        [Test]
         public void Should_create_RoleStore_with_default_connection()
         {
             var target = new RoleStore<IdentityRole>();
@@ -53,6 +65,19 @@ namespace Simple.Data.AspNet.Identity.Tests
         public void Should_create_RoleStore_with_named_connection()
         {
             var target = new RoleStore<IdentityRole>("TestDatabase");
+            var task = target.FindByIdAsync(TestData.Admin_RoleId);
+            task.Wait();
+
+            var role = task.Result;
+            Assert.That(role, Is.Not.Null);
+            Assert.That(role.Id, Is.EqualTo(TestData.Admin_RoleId));
+        }
+
+        [Test]
+        public void Should_create_RoleStore_with_connection_string()
+        {
+            var target = new RoleStore<IdentityRole>(@"Server=(localdb)\v11.0;Database=IdentityTest;Integrated Security=true;Trusted_Connection=True;");
+
             var task = target.FindByIdAsync(TestData.Admin_RoleId);
             task.Wait();
 
--- Simple.Data.AspNet.Identity/Storage.cs
+++ Simple.Data.AspNet.Identity/Storage.cs
@@ -1,3 +1,5 @@
+using System.Text.RegularExpressions;
+
 namespace Simple.Data.AspNet.Identity
 {
     class Storage<TUser, TRole> where TUser : IdentityUser 
@@ -9,6 +11,7 @@ namespace Simple.Data.AspNet.Identity
         private UserRoleTable _userRoleTable;
         private UserClaimsTable _userClaimsTable;
         private UserLoginsTable _userLoginsTable;
+        private readonly Regex _isConnectionString = new Regex("^([^\\\"])([^=;]+=[^=;]+;){1,}(.*[^\\\"]$)", RegexOptions.Compiled);
 
         private readonly dynamic _database;
 
@@ -21,12 +24,25 @@ namespace Simple.Data.AspNet.Identity
         /// <summary>
         /// Constructor that takes connection name and table details to use
         /// </summary>
-        /// <param name="connectionName">Name of connection string in config</param>
+        /// <param name="connection">Name of connection string in config or a valid connection string</param>
         /// <param name="tables"><see cref="Tables"/> holding table details</param>
-        public Storage(string connectionName, Tables tables)
+        public Storage(string connection, Tables tables)
         {
             _tables = tables;
-            _database = connectionName == null ? Database.Open() : Database.OpenNamedConnection(connectionName);
+            if (string.IsNullOrWhiteSpace(connection))
+            {
+                _database = Database.Open();
+            }
+            else
+            {
+                _database = _isConnectionString.IsMatch(connection) ? Database.OpenConnection(connection) : Database.OpenNamedConnection(connection);
+            }
+            
+        }
+
+        private void OpenConnection(string connection)
+        {
+            
         }
 
         public UserTable<TUser> UsersTable
